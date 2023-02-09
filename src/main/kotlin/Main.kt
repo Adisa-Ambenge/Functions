04b5d4ef3d4467190088b07a4dffae026f0d4008
Angela <ambengeangela@gmail.com>
2023-02-09 06:19:51 +0300

    This is my second Kotlin Assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,36 @@
+fun main(){
+    name()
+   message()
+    division()
+    sum()
+
+}
+
+fun name() {
+    var name = "Angela"
+    println("Hello" + " " + name)
+}
+
+fun division() {
+    var num1=85
+    var num2=4
+    var result=num1%num2
+    println(result)
+}
+
+fun sum(){
+    var num1=23
+    var num2=2
+    var num3=6
+    var num4=17
+    var result=num1+num2+num3+num4
+    println(result)
+}
+
+
+
+
+fun message() {
+    var message = ("I am a very patient person!")
+    println(message)
+}
